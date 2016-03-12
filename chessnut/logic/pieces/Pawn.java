a0dab001266d854d7e3cadfb3be6c3130b93753a
icguy <ayhandani@hotmail.com>
2016-03-12 16:15:05 +0100

    implemented pawn movements

--- chessnut/logic/pieces/Pawn.java
+++ chessnut/logic/pieces/Pawn.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import chessnut.logic.*;
 
 public class Pawn extends Piece
-{		
+{
 	public Pawn(PlayerColor color)
 	{
 		super(color);
@@ -23,7 +23,7 @@ public class Pawn extends Piece
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
 	public String toString()
 	{
@@ -33,8 +33,35 @@ public class Pawn extends Piece
 	@Override
 	public ArrayList<Move> getPossibleMoves(Position pos, ChessBoard board)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Move> moves = new ArrayList<>();
+		int pawnDir = color == PlayerColor.White ? 1 : -1;
+		int initRank = color == PlayerColor.White ? 1 : 6;
+		int rank = pos.getRank();
+		int file = pos.getFile();
+
+		Position currPos = new Position(rank + pawnDir, file);
+		Piece currPiece = board.getPiece(currPos);
+		if (currPiece == null)
+		{
+			moves.add(new Move(pos, currPos));	//step 1 forward
+
+			currPos = new Position(rank + 2 * pawnDir, file);
+			currPiece = board.getPiece(currPos);
+			if (pos.getRank() == initRank && currPiece == null)
+				moves.add(new Move(pos, currPos)); //initial step 2 forward
+		}
+
+		currPos = new Position(rank + pawnDir, file + 1);
+		currPiece = board.getPiece(currPos);
+		if(currPiece != null && currPiece.color != color)
+			moves.add(new Move(pos, currPos));	//capture piece
+		
+		currPos = new Position(rank + pawnDir, file - 1);
+		currPiece = board.getPiece(currPos);
+		if(currPiece != null && currPiece.color != color)
+			moves.add(new Move(pos, currPos));	//capture piece
+				
+		return moves;
 	}
-	
+
 }
